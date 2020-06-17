bdda0cab665c16b358d1ec5bd7f305bbd5697fa1
Your Name <you@example.com>
2020-06-16 23:56:30 -0300

    first commit

---
+++ WebApiCoronavirus/Controllers/CoronavirusController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using WebApiCoronavirus.Models;
+
+namespace WebApiCoronavirus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoronavirusController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CoronavirusController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/Coronavirus
+        [HttpGet]
+        public async  Task<ActionResult<EstadisticasCoVid19>> Get()
+        {
+
+            string urlApi = _configuration["ApiCoronaVirus:Url"];
+
+            HttpClient client = new HttpClient();
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(urlApi)
+            };
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+            var responseBodyJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+
+            EstadisticasCoVid19 estadisticas = JsonConvert.DeserializeObject<EstadisticasCoVid19>(responseBodyJson);
+
+
+            return estadisticas;
+        }
+
+        // GET: api/Coronavirus/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Coronavirus
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Coronavirus/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiCoronavirus/Models/EstadisticasCoVid19.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCoronavirus.Models
+{
+    public class EstadisticasCoVid19
+    {
+        public Global Global { get; set; }
+        public IEnumerable<Countries> Countries { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class Global
+    {
+
+        public int NewConfirmed { get; set; }
+
+        public int TotalConfirmed { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public int NewDeaths { get; set; }
+
+        public int TotalDeaths { get; set; }
+
+        public int NewRecovered { get; set; }
+
+        public int TotalRecovered { get; set; }
+
+    }
+
+    public class Countries
+    {
+
+        public string Country { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string Slug { get; set; }
+
+        public int NewConfirmed { get; set; }
+
+        public int TotalConfirmed { get; set; }
+
+        public int NewDeaths { get; set; }
+
+        public int TotalDeaths { get; set; }
+
+        public int NewRecovered { get; set; }
+
+        public int TotalRecovered { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+
+    }
+
+
+}
